b058416301c8a5617fac24ea412192bac62a342a
Ernesto Cervantes <ernesto.acacej@gmail.com>
2020-10-12 01:52:30 -0500

    Update 04_POKEMON_CARDS_CRUD

--- 04_POKEMON_CARDS_CRUD/server/server.js
+++ 04_POKEMON_CARDS_CRUD/server/server.js
@@ -9,31 +9,68 @@ const pokemonsSaved = {}
 app.use(express.json())
 app.use(cors())
 
-app.get('/getPokemon/:id', async (req, res) => {
-    let name = "";
+app.get('/get/:id', async (req, res) => {
     let id = 0;
-    if (req.params.id.replace(/[0-9]/g, '').trim())
-        name = req.params.id.trim();
-    else
-        id = parseInt(req.params.id);
+    id = parseInt(req.params.id);
 
-    if (id in pokemonsSaved || name in pokemonsSaved) {
+    if (id in pokemonsSaved) {
         console.log("Info is in cache.")
     } else {
         console.log("Info is not in cache. Asking pokeapi...")
 
-        await axios.get(`https://pokeapi.co/api/v2/pokemon/${name ? name : id}`)
+        await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
             .then(({ data }) => {
                 pokemonsSaved[data.id] = data
-                pokemonsSaved[data.name] = data
             })
             .catch((err) => {
                 console.error(err.response)
                 return res.sendStatus(err.response.status)
             })
     }
-    res.send(pokemonsSaved[name || id])
+    res.send(pokemonsSaved[id])
     console.log("Sent.")
+})
+
+app.get('/getAll', async (req, res) => {
+    console.log("Sending all... ")
+    res.send(pokemonsSaved)
+    console.log("completed.")
+})
+
+app.post('/create', async (req, res) => {
+    console.log("Saving... ")
+    const { id, ...info } = req.body;
+    pokemonsSaved[id] = info;
+    res.send(pokemonsSaved[id])
+    console.log("completed.")
+})
+
+app.put('/update/:id', async (req, res) => {
+    id = parseInt(req.params.id);
+    
+    if (id in pokemonsSaved) {
+        console.log("Updating... ")
+        pokemonsSaved[id] = {...pokemonsSaved[id] , ...req.body}
+        res.send()
+        console.log("completed.")
+    } else {
+        console.error("Info is NOT in cache.")
+        res.status(400).send()
+    }
+})
+
+app.delete('/delete/:id', async (req, res) => {
+    id = parseInt(req.params.id);
+
+    if (id in pokemonsSaved) {
+        console.log("Info is in cache... ")
+        delete pokemonsSaved[id]
+        res.send()
+        console.log("deleted.")
+    } else {
+        console.error("Info is NOT in cache.")
+        res.status(400).send()
+    }
 })
 
 app.listen(PORT, () => {
